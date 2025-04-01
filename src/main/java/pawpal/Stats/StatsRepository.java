7f48bb8b25ff4cbd92d52adb55e7479d7cd505b8
cicureton <ccinfinity05@gmail.com>
2025-04-01 12:16:13 -0400

    Temporary rename to fix case issue

--- src/main/java/pawpal/Stats/StatsRepository.java
+++
@@ -1,8 +0,0 @@
-package pawpal.Stats;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StatsRepository extends JpaRepository<Stats, Integer>{
-    Stats findByProviderId(int providerId);
-}
